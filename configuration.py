5bcb7966a7ab5365f540b462974d580dce4247a6
Vasiliy Shurpatov <vasiliyshurpatov@gmail.com>
2026-06-29 17:30:51 +0300

    My first commit

---
+++ configuration.py
@@ -0,0 +1,4 @@
+URL_SERVICE = "https://dca10a1f-1f85-4405-b5ab-9c3a2fa23ee0.serverhub.praktikum-services.ru"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+CREATE_USER_PATH = "/api/v1/users/" 
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
